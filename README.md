559c71f92943cb8ec6026aa8685efa5d315d6a3c
andrewhu02 <andrewhu@brandeis.edu>
2024-01-16 22:06:28 -0500

    added name to:   README.md 	added: tictactoe 	edited: App.js to make board of buttons 1-9

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 COSI103 Group J
 Andrew Hu
 andrewhu@brandeis.edu
-Charles Sullivan
-charlessullivan@brandeis.edu
-Nhi Le (nhihllle@brandeis.edu)
---
+++ tictactoe/src/App.js
@@ -0,0 +1,21 @@
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button classname="square">1</button>
+        <button classname="square">2</button>
+        <button classname="square">3</button>
+      </div>
+      <div className="board-row">
+        <button classname="square">4</button>
+        <button classname="square">5</button>
+        <button classname="square">6</button>
+      </div>
+      <div className="board-row">
+        <button classname="square">7</button>
+        <button classname="square">8</button>
+        <button classname="square">9</button>
+      </div>
+    </>
+  )
+}
