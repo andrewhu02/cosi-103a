1f4ce1db97fce8899962006b2317be2b22a8620c
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-20 01:52:48 -0500

    finished tictactoe game

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -5,14 +5,53 @@ function Square({value, onSquareClick}){
 }
 export default function Board(){
   const [squares,setSquares] =useState(Array(9).fill(null));
-  
-  function handleClick(i){
-    const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calclulateWinner(squares);
+  let status;
+
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext? "X" : "O");
   }
+
+   function calclulateWinner(squares){
+    const lines=[
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],    
+    ];
+    for(let i = 0; i <lines.length;i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+   }
+
+  function handleClick(i){
+    if(squares[i]||calclulateWinner(squares)){
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext){
+       nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
   <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
       <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
