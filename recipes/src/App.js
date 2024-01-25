4c63e31562684bf36b6f269c6670b8d3a4239da4
Nhi Le <halannhile@gmail.com>
2024-01-25 17:21:38 -0500

    added favicon, added recipe body

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -27,15 +27,144 @@ function RecipeAccordion() {
   return (
     <Accordion defaultActiveKey={0}>
       <Accordion.Item eventKey='0'>
-        <Accordion.Header>Accordion Header Text</Accordion.Header>
+        <Accordion.Header>Recipe 1</Accordion.Header>
         <Accordion.Body>
-          Accordion body text.
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
+
       <Accordion.Item eventKey='1'>
-        <Accordion.Header>Accordion Header Text</Accordion.Header>
+        <Accordion.Header>Recipe 2</Accordion.Header>
         <Accordion.Body>
-          Accordion body text.
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='2'>
+        <Accordion.Header>Recipe 3</Accordion.Header>
+        <Accordion.Body>
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='3'>
+        <Accordion.Header>Recipe 4</Accordion.Header>
+        <Accordion.Body>
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='4'>
+        <Accordion.Header>Recipe 5</Accordion.Header>
+        <Accordion.Body>
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='5'>
+        <Accordion.Header>Recipe 6</Accordion.Header>
+        <Accordion.Body>
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='6'>
+        <Accordion.Header>Recipe 7</Accordion.Header>
+        <Accordion.Body>
+          <img src="https://design4users.com/wp-content/uploads/2023/03/food-illustration-helen-lee.jpg" alt="Recipe Image" class="center scaled-image padded-image"/>
+          <div class="container">
+            <div class="column1">
+                <h2>Ingredients</h2>
+                <ul>
+                  <li>ingredient 1</li>
+                  <li>ingredient 2</li>
+                  <li>ingredient 3</li>
+                </ul>
+            </div>
+            <div class="column2">
+                <h2>Cooking method</h2>
+                <p>This is how to cook this dish...</p>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
