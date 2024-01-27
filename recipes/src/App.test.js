e305eab3c9263748a01eb65fad7ebda61c9ccc04
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-01-27 22:52:40 +0000

    correctly edited test file to intentionally fail

--- recipes/src/App.test.js
+++ recipes/src/App.test.js
@@ -3,8 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/abcd/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-fail('on purpose')
