49be7697dd3c8c796c77ac564b93a4d0377b9a9b
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-01-27 22:54:42 +0000

    edited tests to pass

--- recipes/src/App.test.js
+++ recipes/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/abcd/i);
+  const linkElement = screen.getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
