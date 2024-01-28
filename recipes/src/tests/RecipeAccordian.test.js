6a57cfedb2a9c2f6d12777a9eecb2fafe87e5016
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-28 18:15:23 -0500

    refined the testing for rendering and interaction

--- recipes/src/tests/RecipeAccordian.test.js
+++ recipes/src/tests/RecipeAccordian.test.js
@@ -1,14 +1,24 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import RecipeAccordion from '../App';
 
 test('check recipe accordian', () => {
     render(<RecipeAccordion />);
-    const recipeChild = screen.getByText('Recipe 1').parentNode;
-    expect(recipeChild).toBeInTheDocument();
+    const  recipesAccordian= screen.getByText('Recipe 1').parentNode.parentNode.parentNode;
+    // >>>>> !!!<accordion>!!! <accordion.item> <accordion.head> 'Recipe 1 '
+    expect(recipesAccordian).toBeInTheDocument();
   });
   test('check recipe 1', () => {
     render(<RecipeAccordion />);
-    const recipeChild = screen.getByText('Recipe 1');
-    expect(recipeChild).toBeInTheDocument();
-    expect
+    const recipeHead = screen.getByText('Recipe 1').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
   });
