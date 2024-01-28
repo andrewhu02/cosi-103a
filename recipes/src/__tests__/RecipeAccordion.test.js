d7528c68060d8f3247bcead996695cd926465b47
Nhi Le <halannhile@gmail.com>
2024-01-28 14:25:41 -0500

    set up tests with Jest

---
+++ recipes/src/__tests__/RecipeAccordion.test.js
@@ -0,0 +1,24 @@
+// src/__tests__/RecipeAccordion.test.js
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import RecipeAccordion from '../RecipeAccordion';
+
+test('renders RecipeAccordion component with Recipe 1', () => {
+  const { getByText } = render(<RecipeAccordion />);
+  
+  // Assertions for Recipe 1
+  expect(getByText('Recipe 1')).toBeInTheDocument();
+  expect(getByText('2 lbs chicken thighs')).toBeInTheDocument();
+  expect(getByText('1/4 cup olive oil')).toBeInTheDocument();
+});
+
+test('renders RecipeAccordion component with Recipe 2', () => {
+  const { getByText } = render(<RecipeAccordion />);
+  
+  // Assertions for Recipe 2
+  expect(getByText('Recipe 2')).toBeInTheDocument();
+  expect(getByText('ingredient 1')).toBeInTheDocument();
+  expect(getByText('This is how to cook this dish...')).toBeInTheDocument();
+});
+
