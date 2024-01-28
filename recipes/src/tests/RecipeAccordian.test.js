ba9a26e5f742619eaade0d502cb8742f4c167267
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-28 18:21:20 -0500

    further editing, created test for all recipe items

--- recipes/src/tests/RecipeAccordian.test.js
+++ recipes/src/tests/RecipeAccordian.test.js
@@ -18,6 +18,110 @@ test('check recipe accordian', () => {
     */
     expect(recipeHead).toBeInTheDocument();
     expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 2', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 2').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 3', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 3').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 4', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 4').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 5', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 5').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 6', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 6').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
+    fireEvent.click(recipeHead,()=>{
+        expect(recipeBody).toBeVisible();
+    })
+  });
+  test('check recipe 7', () => {
+    render(<RecipeAccordion />);
+    const recipeHead = screen.getByText('Recipe 7').parentNode;
+    const recipeBody = recipeHead.parentNode.parentNode.children[1];
+    /*
+        <accordian.item>
+            <accordian.head>
+        >>>><accordian.body>    
+    */
+    expect(recipeHead).toBeInTheDocument();
+    expect(recipeBody).toBeInTheDocument();
+    !expect(recipeBody).toBeVisible();
+    //checks if clicking on the title will show the content
     fireEvent.click(recipeHead,()=>{
         expect(recipeBody).toBeVisible();
     })
