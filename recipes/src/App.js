7f1bdcae559e6d1183be9f12cb770ffb5a653e37
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-28 20:11:38 -0500

    added chocolate chip recipes with ingredients and instructions

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -129,14 +129,33 @@ function RecipeAccordion() {
             <div class="column1">
                 <h2 class = "subheader">Ingredients</h2>
                 <ul>
-                  <li>ingredient 1</li>
-                  <li>ingredient 2</li>
-                  <li>ingredient 3</li>
+                  <li>3 cups of all purpose flour</li>
+                  <li>1/2 tsp of baking powder</li>
+                  <li>1/4 tsp of salt</li> 
+                  <li>1/4 tsp of espresso powder</li>
+                  <li>1 cup of plain white sugar</li>
+                  <li>1 cup of brown sugar or molasses</li>
+                  <li>1 cup of melted butter</li>
+                  <li>2 eggs</li>
+                  <li>1 tsp of vanilla extract</li>
+                  <li>2 cups of chocolate chips/chunks</li>
                 </ul>
             </div>
             <div class="column2">
                 <h2 class = "subheader">Cooking Instructions</h2>
-                <p>This is how to cook this dish...</p>
+                <ol>
+                  <li>Preheat the oven to 350 degrees, or you can start preheating after step 7, depends on your pace</li>
+                  <li>If you haven’t already, prepare the melted butter by either melting it at a low heat in a pan on stove or melting it in the microwave</li>
+                  <li>Cream the butter with the white sugar and brown sugar/molasses until both incorporated or about 2 - 3 minutes</li>
+                  <li>Mix the eggs, with egg yolk and white, and the vanilla extract into the wet mixture</li>
+                  <li>In a separate bowl combine and sift the dry ingredients (flour, salt, baking powder, espresso powder)</li>
+                  <li>Gradually add in the dry ingredients into the wet mixture until incorporated</li>
+                  <li>Stir in the chocolate chips until incorporated</li>
+                  <li>While oven is preheating, shape small balls out of the dough and place on a 9 x 13 pan, 16 is usually expected</li>
+                  <li>Once the oven is preheated, bake the cookies until 12 - 15 minutes</li>
+                  <li>Take the cookies out of the oven and let them cool for at least 10 minutes</li>
+                  <li>Enjoy!</li>
+                </ol>
             </div>
           </div>
         </Accordion.Body>
