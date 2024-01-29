ca0387bacbaeb485260dd5a6cfd07c08456b8ae0
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-28 19:39:57 -0500

    can now properly check if the button works

--- recipes/src/__tests__/RecipeAccordion.test.js
+++ recipes/src/__tests__/RecipeAccordion.test.js
@@ -15,94 +15,101 @@ test('check recipe accordion', () => {
   const recipesAccordion = screen.getByText('Recipe 1').parentNode.parentNode.parentNode;
   expect(recipesAccordion).toBeInTheDocument();
 });
-
+//recipe 1 is always shown initially
 test('check recipe 1', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 1').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).not.toHaveClass('show');
+  });
 });
 
 test('check recipe 2', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 2').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
 
 test('check recipe 3', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 3').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
 
 test('check recipe 4', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 4').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
 
 test('check recipe 5', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 5').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
 
 test('check recipe 6', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 6').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
 
 test('check recipe 7', () => {
   render(<RecipeAccordion />);
   const recipeHead = screen.getByText('Recipe 7').parentNode;
-  const recipeBody = recipeHead.parentNode.children[1].firstChild;
+  const recipeBody = recipeHead.parentNode.children[1];
   expect(recipeHead).toBeInTheDocument();
   expect(recipeBody).toBeInTheDocument();
-  // expect(recipeBody).not.toBeVisible();
+  expect(recipeBody).not.toHaveClass('show');
 
   // Check if clicking on the title will show the content
-  fireEvent.click(recipeHead);
-  // expect(recipeBody).toBeVisible();
+  fireEvent.click(recipeHead,()=>{
+    expect(recipeBody).toHaveClass('show');
+  });
 });
