06f5c466dfbc92d4f40521a97bf911123911b7bc
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-01-28 23:08:31 -0500

    added image of nigerian meat pie, added in nigerian meat pie recipe

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -4,7 +4,7 @@ import Accordion from 'react-bootstrap/Accordion';
 import chickenandrice from './img/chickenandrice.jpg';
 import food from './img/food.jpg';
 import chocchip from './img/choc_chip_cookies.jpg';
-
+import nmeatpie from './img/nigerian_meatpie.jpg';
 
 function App() {
   return (
@@ -104,19 +104,67 @@ function RecipeAccordion() {
       <Accordion.Item eventKey='3'>
         <Accordion.Header>Recipe 4</Accordion.Header>
         <Accordion.Body>
-          <img src={food} alt="Sample Recipe" class="center scaled-image padded-image"/>
+          <img src={nmeatpie} alt="Sample Recipe" class="center scaled-image padded-image"/>
           <div class="container">
             <div class="column1">
                 <h2 class = "subheader">Ingredients</h2>
+                <h4>Dough</h4>
                 <ul>
-                  <li>ingredient 1</li>
-                  <li>ingredient 2</li>
-                  <li>ingredient 3</li>
+                  <li>4.5 cups of all purpose flour </li>
+                  <li>2 tsp of baking powder</li>
+                  <li>2 cups of butter</li>
+                  <li>up to 1 tsp of salt</li>
+                  <li>1/2 cup of cold water</li>
+                </ul>
+                <h4>Filling</h4>
+                <ul>
+                  <li>2 medium-sized potatoes</li>
+                  <li>2 medium-sized carrots</li>
+                  <li>1 pound of ground meat(turkey, beef)</li>
+                  <li>1 medium-sized onion</li>
+                  <li>2 tablespoons of vegetable oil</li>
+                  <li>2 cubes/2 tablespoons of bouillon</li>
+                  <li>1 stem/1 tsp of thyme</li>
+                  <li>2 tablespoon of plain flour</li>
+                  <li>1 cup of cold water</li>
+                  <li>salt to taste</li>
                 </ul>
             </div>
             <div class="column2">
                 <h2 class = "subheader">Cooking Instructions</h2>
-                <p>This is how to cook this dish...</p>
+                <p>You can start making the filling or dough, make sure to refridgerate the other while you're making either one</p>
+                <h4>Making Dough</h4>
+                <ol>
+                  <li>In a bowl, add all dry ingredients(flour, baking powder, salt) and mix together</li>
+                  <li>Cut butter in small bits and add into the mixture, using hands to crush until it crumbles</li>
+                  <li>Slowly add water while folding the dough into itself in the bowl</li>
+                  <li>Take the dough out of ball and knead for about 5- 7 minutes. Leave to rest</li>
+                </ol>
+                <h4>Making Filling</h4>
+                <ol>
+                  <li>Prepare and cut potatoes, carrots and onions if you had not already.</li>
+                  <li>Set oil onto the pot and set stove to medium heat.</li>
+                  <li>Once hot enough, add in the onions</li>
+                  <li>Once the onions are translucent, add in the meat and stir until pale brown.</li>
+                  <li>Add in a cup of water, bouillon, and thyme, and stir in until combined.</li>
+                  <li>Cover the pot with increased heat, once its boiling, add in the carrots and potatoes. Lower the heat</li>
+                  <li>Stir in the flour to the filling.</li>
+                  <li>Let it cook for several more minutes, add in salt to taste.</li>
+                  <li>Once cooked, turn off the heat and set the filling aside</li>
+                </ol>
+                <h4>Making the Meat Pie</h4>
+                <ol>
+                  <li>Preheat the oven to 335°C</li>
+                  <li>Cover a 9 x 13 tray in butter or use a baking sheet</li>
+                  <li>Knead the dough and roll out into a up to 2inches</li>
+                  <li>Cut out circles from the dough with a cutter or a knife</li>
+                  <li>Add in filling in the center, try not to overfill</li>
+                  <li>Add egg wash around the interior edge and fold the dough over the filling and seal it with a fork</li>
+                  <li>Place all the meat pie onto the tray and cover with egg wash</li>
+                  <li>Bake for about 30-40 minutes</li>
+                  <li>Let it cool and enjoy!</li>
+                
+                </ol>
             </div>
           </div>
         </Accordion.Body>
@@ -144,7 +192,7 @@ function RecipeAccordion() {
             <div class="column2">
                 <h2 class = "subheader">Cooking Instructions</h2>
                 <ol>
-                  <li>Preheat the oven to 350 degrees, or you can start preheating after step 7, depends on your pace</li>
+                  <li>Preheat the oven to 350°C degrees, or you can start preheating after step 7, depends on your pace</li>
                   <li>If you haven’t already, prepare the melted butter by either melting it at a low heat in a pan on stove or melting it in the microwave</li>
                   <li>Cream the butter with the white sugar and brown sugar/molasses until both incorporated or about 2 - 3 minutes</li>
                   <li>Mix the eggs, with egg yolk and white, and the vanilla extract into the wet mixture</li>
