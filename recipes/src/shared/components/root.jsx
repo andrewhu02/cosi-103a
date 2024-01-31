c2d26fb2059498f0d3c7ea7c55038274b859ffa3
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-01-31 23:44:34 +0000

    automatic redirect to /homepage

--- recipes/src/shared/components/root.jsx
+++ recipes/src/shared/components/root.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Header from "./header/Header"
 import MainNavigation from "./navigation/MainNavigation"
+import { useEffect } from "react"
 
 export default function Root() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const shouldRedirect = (window.location.pathname == "/")
+        if(shouldRedirect) {
+            navigate("/homepage");
+        }
+    }, [navigate]);
     return (
         <>
             <Header/>
