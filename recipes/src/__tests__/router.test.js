5a67b869d438d52a6d921e64194c77c5c8a7c041
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-02-03 18:07:07 +0000

    working on tests

--- recipes/src/__tests__/router.test.js
+++ recipes/src/__tests__/router.test.js
@@ -4,14 +4,26 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import App from "../App"
 
-test('app rendering/navigating', async() => {
+test('app rendering/navigating with navbar', async() => {
     render(<App />)
     const user = userEvent.setup()
     
-    // verify redirect to/homepage
-    expect(screen.getByText(/this is the home page/i)).toBeInTheDocument()
+    // verify redirect to /homepage
+    // look for the test text because that is only on homepage, but
+    // should update with different text once 'test' is replaced
+    expect(screen.getAllByText((/test/i))[0]).toBeInTheDocument()
+
+    // verify 'Home' button navigates to home page
+    await user.click(screen.getByText(/home/i))
+    expect(screen.getAllByText((/test/i))[0]).toBeInTheDocument()
+
+    // verify 'All recipes' button navigates to all recipes page
+    await user.click(screen.getByRole('navigation', { name: /recipes/i}))
+    await user.click(screen.getByText(/all recipes/i))
+    expect(screen.getByRole('heading', { name: /this page shows all recipes/i })).toBeInTheDocument()
 
     // verify 'About Us' button navigates to About Us page
     await user.click(screen.getByText(/about us/i))
     expect(screen.getByRole('heading', { name: /about us/i })).toBeInTheDocument()
+    
 })
