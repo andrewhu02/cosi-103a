e6193d1b9acf088cb07f8f191b44e2453a948d3e
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-02-13 17:51:23 +0000

    testing opening and closing of list

--- recipes/src/__tests__/GroceryList.test.js
+++ recipes/src/__tests__/GroceryList.test.js
@@ -1,28 +1,38 @@
-import { act } from "react-dom/test-utils"
-import App from "../App"
-import { ReactDOM } from "react-dom"
-import '@testing-library/jest-dom'
-// import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import React from 'react'
+import { render, fireEvent, screen, logRoles } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import App from '../App'
+import userEvent from '@testing-library/user-event'
 
-let el = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
-  el = document.createElement('div');
-  // container *must* be attached to document so events work correctly.
-  document.body.appendChild(el);
+  // workaround for error when testing
+  // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 });
 
 test('Grocery List button opens Offcanvas list', async () => {
-    await act(async () => {
-        ReactDOM.render(<App />, el)
-    });
-    expect(screen.getAllByText((/home/i))[0]).toBeInTheDocument()
+  const user = userEvent.setup()
+  render(<App />)
+  await user.click(screen.getByRole('button', { name: /grocery list/i }))
+  expect(screen.getByRole('dialog', { show: true}))
 })
-// test('clicking the \'X\' closes the list')
-// test('clicking outside of the list closes it') // maybe
-// test('clicking the \'Increment\' button updates the state of the list')
-// test('GroceryList maintains state when closed and reopened')
-// test('GroceryList maintains state when page is changed')
+
+test('clicking the \'X\' closes the list', async () => {
+  const user = userEvent.setup()
+  render(<App />)
+  await user.click(screen.getByRole('button', { name: /grocery list/i }))
+  await user.click(screen.getByRole('button', { name: /close/i }))
+  expect(screen.getByRole('dialog', { show: false}))
+})
