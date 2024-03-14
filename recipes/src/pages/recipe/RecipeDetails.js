4837d2fe1016c21d13809b5a1ff86ba1c8619354
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-03-14 00:45:41 +0000

    fetch recipes w/ useState

--- recipes/src/pages/recipe/RecipeDetails.js
+++ recipes/src/pages/recipe/RecipeDetails.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const RecipeDetails = ({ recipes }) => {
-  return (
-    <div>
-      {recipes.map((recipe, index) => (
-        <div key={index}>
-          <h1>{recipe.title}</h1>
-          <p style={{ whiteSpace: 'pre-line' }}>{recipe.recipeText}</p>
-          {/* TODO: Add more styling later */}
-        </div>
-      ))}
-    </div>
-  );
-};
+// const RecipeDetails = ({ recipes }) => {
+//   return (
+//     <div>
+//       {recipes.map((recipe, index) => (
+//         <div key={index}>
+//           <h1>{recipe.title}</h1>
+//           <p style={{ whiteSpace: 'pre-line' }}>{recipe.recipeText}</p>
+//           {/* TODO: Add more styling later */}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
 // first, get this page to display all recipes from the API
-async function AllDetails() {
+const AllDetails = () => {
+  // store fetched recipes 
+  const [recipes, setRecipes] = useState([])
   // GET list of recipes from API
-  const recipes = fetch('/api/recipes', {method: 'GET'})
+  useEffect(() => {
+    fetch('/api/recipes', {method: 'GET'})
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // set state of recipes here
+    });
+  }, []);
+  // return recipe data
+  // TODO: add formatting
   return (
-    <p>{recipes}</p>
-    // next: figure out how to display recipes from API
-    // we correctly get the JSON data with all the recipes from this request
+    <div>Check console for data.</div>
   )
 }
 // then, work on using the FoodData API/parsing recipe details to get ingredient links
-
-// would help to have auto-reload somehow when developing
-
 export default AllDetails;
