d1538ebc0f9adeb4a377344863e84507a4a331bf
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-03-14 00:59:26 +0000

    formatting and comments

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -80,7 +80,7 @@ export default function App() {
         },
         {
           path: '/recipe-details',
-          element: <AllDetails />,
+          element: <RecipeDetails />,
         }
       ],
     },
--- recipes/src/pages/recipe/RecipeDetails.js
+++ recipes/src/pages/recipe/RecipeDetails.js
@@ -1,44 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
-// const RecipeDetails = ({ recipes }) => {
-//   return (
-//     <div>
-//       {recipes.map((recipe, index) => (
-//         <div key={index}>
-//           <h1>{recipe.title}</h1>
-//           <p style={{ whiteSpace: 'pre-line' }}>{recipe.recipeText}</p>
-//           {/* TODO: Add more styling later */}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// first, get this page to display all recipes from the API
-const AllDetails = () => {
+// display all recipes from the API
+const RecipeDetails = () => {
   // used to store fetched recipes 
   const [recipes, setRecipes] = useState([])
   // GET list of recipes from API
   useEffect(() => {
-    fetch('/api/recipes', {method: 'GET'})
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      // the API returns an array of Objects, each is a recipe
-      setRecipes(data);
-    });
+    fetch('/api/recipes', { method: 'GET' })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // the API returns an array of Objects, each is a recipe
+        setRecipes(data);
+      });
   }, []);
-  // return recipe data
-  // TODO: add formatting
+  // return element which displays recipes
   return (
     <div>
       {recipes.map((recipe) => (
+        // modify this to display nicely formatted data for each recipe
         <p>{JSON.stringify(recipe)}</p>
       ))}
     </div>
   )
 }
-// then, work on using the FoodData API/parsing recipe details to get ingredient links
-export default AllDetails;
+
+export default RecipeDetails;
