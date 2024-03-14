17bd69f6a383a9f2199b9b70998ff8564b674a1c
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-03-14 15:50:34 +0000

    default recipe data json file, added ingredients and instructions

--- recipes/server/index.js
+++ recipes/server/index.js
@@ -5,19 +5,21 @@ const app = express();
 const PORT = process.env.PORT || 3002; // i accidentally screwed up port 3001 and decided to use 3002 (didn't restart 3001), but you can switch it back to 3001 when you want to test it
 const path = require('path');
 
+const recipes = require("./recipes.json");
+
 app.use(bodyParser.json());
 
 // mock data for initial recipes
-let recipes = [
-  { id: 1, title: 'Halal Guys Chicken and Rice', description: 'A delicious street food dish from NYC.', imageSrc: 'img/food/chickenandrice.jpg', url: '/recipe1'},
-  { id: 2, title: 'Guacamole', description: 'A traditional Mexican dip.', imageSrc: 'img/food/guac.jpg', url: '/recipe2' },
-  { id: 3, title: 'Pasta with Pesto', description: 'A classic Italian dish.', imageSrc: 'img/food/pesto.jpg', url: '/recipe3'},
-  { id: 4, title: 'Nigerian Meat Pies', description: 'Savory pastry snacks filled with seasoned minced meat.', imageSrc: 'img/food/nigerian_meatpie.jpg', url: '/recipe4' },
-  { id: 5, title: 'Chocolate Chip Cookies', description: 'A classic chocolatey sweet indulgence.', imageSrc: 'img/food/choc_chip_cookies.jpg', url: '/recipe5' },
-  { id: 6, title: 'Creole Jambalaya', description: 'A soulful and savory dish deeply rooted in Louisiana\'s culinary culture.', imageSrc: 'img/food/jambalaya.jpg', url: '/recipe6' },
-  { id: 7, title: 'Soup Dumplings', description: 'A revered Chinese delicacy.', imageSrc: 'img/food/soup_dumplings.jpg', url: '/recipe7'},
-  { id: 8, title: 'Pho', description: 'A beloved Vietnamese dish.', imageSrc: 'img/food/pho.jpg', url: '/recipe8'},
-];
+// let recipes = [
+//   { id: 1, title: 'Halal Guys Chicken and Rice', description: 'A delicious street food dish from NYC.', imageSrc: 'img/food/chickenandrice.jpg', ingredients: ["2 lbs chicken thighs", "1/4 cup olive oil", "2 tablespoons lemon juice"], cookingInstructions: ['Cut the chicken beforehand to cook it more evenly and avoid staining the cutting board', 'In a bowl, add the olive oil, lemon juice, vinegar, garlic cloves, all the spices, and the salt and pepper for a chicken marinade.'], url: '/recipe1'},
+//   { id: 2, title: 'Guacamole', description: 'A traditional Mexican dip.', imageSrc: 'img/food/guac.jpg', url: '/recipe2' },
+//   { id: 3, title: 'Pasta with Pesto', description: 'A classic Italian dish.', imageSrc: 'img/food/pesto.jpg', url: '/recipe3'},
+//   { id: 4, title: 'Nigerian Meat Pies', description: 'Savory pastry snacks filled with seasoned minced meat.', imageSrc: 'img/food/nigerian_meatpie.jpg', url: '/recipe4' },
+//   { id: 5, title: 'Chocolate Chip Cookies', description: 'A classic chocolatey sweet indulgence.', imageSrc: 'img/food/choc_chip_cookies.jpg', url: '/recipe5' },
+//   { id: 6, title: 'Creole Jambalaya', description: 'A soulful and savory dish deeply rooted in Louisiana\'s culinary culture.', imageSrc: 'img/food/jambalaya.jpg', url: '/recipe6' },
+//   { id: 7, title: 'Soup Dumplings', description: 'A revered Chinese delicacy.', imageSrc: 'img/food/soup_dumplings.jpg', url: '/recipe7'},
+//   { id: 8, title: 'Pho', description: 'A beloved Vietnamese dish.', imageSrc: 'img/food/pho.jpg', url: '/recipe8'},
+// ];
 
 // endpoint to get all recipes
 app.get('/api/recipes', (req, res) => {
--- recipes/src/pages/recipe/RecipePageTemplate.js
+++ recipes/src/pages/recipe/RecipePageTemplate.js
@@ -1,5 +1,6 @@
 // takes a correctly formatted recipe object
 // and returns a recipe page
+
 // TODO: image formatting
 export default function RecipePage({recipe}) {
     return (
@@ -7,7 +8,16 @@ export default function RecipePage({recipe}) {
             <h1>{recipe.title}</h1>
             <img src={recipe.imageSrc} className="center scaled-image padded-image" />
             <p>{recipe.description}</p>
+            <ul>
+            {recipe.ingredients.map((ingredient) => (
+                <li>{ingredient}</li>
+            ))}
+            </ul>
+            <ol>
+                {recipe.cookingInstructions.map((step) => (
+                    <li>{step}</li>
+                ))}
+            </ol>
         </div>
-        
     )
 }
