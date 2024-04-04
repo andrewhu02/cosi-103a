4588c9143288062858260dbd2cb5a7643977e41a
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-04-04 22:29:42 +0000

    set parameters from container app tags and use them in workflow

--- scripts/blue-green.sh
+++ scripts/blue-green.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# build image for this commit
-docker buildx build -t recipes-image recipes/
-
-# upload image to registry
-az acr login --name cosi103test
-docker tag recipes-image cosi103test.azurecr.io/recipes-image:$GREEN_COMMIT_ID
-docker image push cosi103test.azurecr.io/recipes-image:$GREEN_COMMIT_ID
-
-# deploy new revision to container
-# eventually this will somehow alternate between green and blue
-az containerapp update --name recipes \
-  --resource-group cosi-103a-test \
-  --image cosi103test.azurecr.io/recipes-image:$GREEN_COMMIT_ID \
-  --revision-suffix $GREEN_COMMIT_ID  \
-  --set-env-vars REVISION_COMMIT_ID=$GREEN_COMMIT_ID
+# # deploy new revision to container app
+# az containerapp update --name recipes \
+#   --resource-group cosi-103a-test \
+#   --image cosi103test.azurecr.io/recipes-image:$CURRENT_COMMIT_ID \
+#   --revision-suffix $CURRENT_COMMIT_ID  \
+#   --set-env-vars REVISION_COMMIT_ID=$CURRENT_COMMIT_ID
 
 # label that new revision with the green label
 # this will not work if a revision already has the 'green' label
@@ -23,7 +14,7 @@ az containerapp revision label add \
     --name recipes \
     --resource-group cosi-103a-test \
     --label green \
-    --revision recipes--$GREEN_COMMIT_ID
+    --revision recipes--$CURRENT_COMMIT_ID
 
 # runs tests on that revision
 
--- scripts/set-params.sh
+++ scripts/set-params.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "current_commit_id=$(git rev-parse --short $GITHUB_SHA)" >> $GITHUB_OUTPUT
+
+# get the tags of the current deployment - this is in json format
+tags = $(az containerapp show -g cosi-103a-test -n recipes --query tags)
+
+# save tags to output
+echo "blue_commit_id=$($tags | jq '.blueCommitId')" >> $GITHUB_OUTPUT
+echo "green_commit_id=$($tags | jq '.greenCommitId')" >> $GITHUB_OUTPUT
+echo "production_label=$($tags | jq '.productionLabel')" >> $GITHUB_OUTPUT
