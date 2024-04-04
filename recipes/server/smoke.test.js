53ba4d87e07e74abedd020f686b08fd907ef8b3d
aerhab19 <156860868+aerhab19@users.noreply.github.com>
2024-04-04 17:45:06 -0400

    added basic code to tests

--- recipes/server/smoke.test.js
+++ recipes/server/smoke.test.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const request = require('supertest');
-const router = require('./Recipes');
+const recipeRouter = require('./Recipes');
+const reactRouter = require('./ReactApp');
 
 const app = express();
-app.use('/', router);
+app.use('/', recipeRouter);
+app.use('/', reactRouter);
+
+
 
 describe('checking rendering',()=>{
     //check home page
-    it('check home page',()=>{
-
+    it('check home page',async ()=>{
+        const resp = await request(app).get('/homepage')
+        expect(resp.statusCode).toBe(200)
+        expect(resp.headers['content-type']).toEqual('text/html; charset=UTF-8');
+        
     })
     //check recipe page
-    it('check recipe page',()=>{
-
+    it('check recipe page', async()=>{
+        const resp = await request(app).get('/add-new-recipe')
+        expect(resp.statusCode).toBe(200)
+        expect(resp.headers['content-type']).toEqual('text/html; charset=UTF-8');
     })
     //check add recipe page
-    it('check add recipe page',()=>{
-
+    it('check add recipe page',async ()=>{
+        resp = await request(app).get('/all-recipes')
+        expect(resp.statusCode).toBe(200)
+        expect(resp.headers['content-type']).toEqual('text/html; charset=UTF-8');
     })
     //
 })
