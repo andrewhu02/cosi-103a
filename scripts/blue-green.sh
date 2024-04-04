2541e9ee088417860e8ab5a9feb4d92f7347cda4
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-04-04 18:45:25 +0000

    added step to authenticate with registry

--- scripts/blue-green.sh
+++ scripts/blue-green.sh
@@ -4,6 +4,7 @@
 docker buildx build -t recipes-image recipes/
 
 # upload image to registry
+az acr login --name cosi103test
 docker tag recipes-image cosi103test.azurecr.io/recipes-image:$GREEN_COMMIT_ID
 docker image push cosi103test.azurecr.io/recipes-image:$GREEN_COMMIT_ID
 
