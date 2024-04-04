fdedfeb242dd8466461f21722ff06cef0210e0bc
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-04-04 17:41:02 +0000

    modify script to build, upload, deploy new docker image

--- scripts/blue-green.sh
+++ scripts/blue-green.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# build image for this commit
+docker buildx build -t $GREEN_COMMIT_ID recipes/
+
+# upload image to registry
+docker tag $GREEN_COMMIT_ID cosi103test.azurecr.io/repository
+docker image push cosi103test.azurecr.io/repository
+
 # deploy new revision to container
 # eventually this will somehow alternate between green and blue
 az containerapp update --name recipes \
   --resource-group cosi-103a-test \
-  --image cosi103test.azurecr.io/github-action/container-app:8440849078.1 \
+  --image cosi103test.azurecr.io/repository:$GREEN_COMMIT_ID \
   --revision-suffix $GREEN_COMMIT_ID  \
   --set-env-vars REVISION_COMMIT_ID=$GREEN_COMMIT_ID
 
@@ -20,8 +27,9 @@ az containerapp revision label add \
 # runs tests on that revision
 
 # if tests pass:
-  # re-route traffic to green
-  # set up labels to swap correctly
+# set 100% of traffic to green revision
+az containerapp ingress traffic set --name recipes --resource-group cosi-103a-test --label-weight blue=0 green=100
+  
 # if tests fail
   # fail deploy action
   # shouldn't need to do anything else here
