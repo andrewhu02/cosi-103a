effcd5b63c5838f93e42ec47a4c0effe5ca6a541
Charles Sullivan <156863333+charliebsulli@users.noreply.github.com>
2024-04-05 15:04:48 +0000

    add -r as arg for jq to remove quotes from commit hashes

--- scripts/set-params.sh
+++ scripts/set-params.sh
@@ -6,12 +6,12 @@ echo "current_commit_id=$(git rev-parse --short $GITHUB_SHA)" >> $GITHUB_OUTPUT
 tags=$(az containerapp show -g cosi-103a-test -n recipes --query tags)
 
 # save tags to output
-blue_commit_id=$(echo $tags | jq '.blueCommitId')
-green_commit_id=$(echo $tags | jq '.greenCommitId')
+blue_commit_id=$(echo $tags | jq -r '.blueCommitId')
+green_commit_id=$(echo $tags | jq -r '.greenCommitId')
 echo "blue_commit_id=$blue_commit_id" >> $GITHUB_OUTPUT
 echo "green_commit_id=$green_commit_id" >> $GITHUB_OUTPUT
 
-production_label=$(echo $tags | jq '.productionLabel')
+production_label=$(echo $tags | jq -r '.productionLabel')
 echo "production_label=$production_label" >> $GITHUB_OUTPUT
 
 if [ $production_label = 'blue' ]; then
