b4fa2ee96d6b97bb7d6087657007b1af2606ebde
andrewhu02 <andrewhu@brandeis.edu>
2024-04-07 22:24:49 -0400

    Added edit function that does not persist with page reload TODO: Add deletion and incorporate backend to make changes persist

--- recipes/src/pages/recipe-api/RecipeDetails.js
+++ recipes/src/pages/recipe-api/RecipeDetails.js
@@ -1,37 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import {Row, Col, Container} from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import RecipePageTemplate from './RecipePageTemplate';
 
-// Display all recipes from the API
 const RecipeDetails = () => {
-  // Used to store fetched recipes 
   const [recipes, setRecipes] = useState([]);
 
-  // GET list of recipes from API
   useEffect(() => {
     fetch('/api/recipes', { method: 'GET' })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // The API returns an array of objects, each representing a recipe
-        setRecipes(data);
-      });
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
   }, []);
+
+  const handleUpdateRecipeData = (updatedRecipe) => {
+    const updatedRecipes = recipes.map((r) => (r.id === updatedRecipe.id ? updatedRecipe : r));
+    setRecipes(updatedRecipes);
+  };
+
+  const handleDeleteRecipe = (recipeId) => {
+    fetch(`/api/recipes/${recipeId}`, { method: 'DELETE' })
+      .then(() => {
+        const updatedRecipes = recipes.filter((recipe) => recipe.id !== recipeId);
+        setRecipes(updatedRecipes);
+      })
+      .catch((error) => console.error('Error deleting recipe:', error));
+  };
 
   return (
     <Container>
       <Row>
         {recipes.map((recipe, index) => (
           <Col md={6} key={index}>
-            {/* Use the RecipePageTemplate component for each recipe */}
-            <RecipePageTemplate recipe={recipe} />
+            <RecipePageTemplate recipe={recipe} onUpdateRecipe={handleUpdateRecipeData} onDeleteRecipe={handleDeleteRecipe} />
           </Col>
         ))}
       </Row>
     </Container>
   );
-}
+};
 
 export default RecipeDetails;
